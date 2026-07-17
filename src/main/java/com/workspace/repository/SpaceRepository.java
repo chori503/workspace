62ec8f13f32e12b569bf73e106ceaab8d2ca59ab
Jorge Romero <jorgearomero123@gmail.com>
2026-07-17 01:09:19 -0600

    Revert "Agregando crud de spaces"
    This reverts commit eae96ae82f3762006b7b722c54a02cffc724a0bd.

--- src/main/java/com/workspace/repository/SpaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.workspace.repository;
-
-import com.workspace.entity.Space;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpaceRepository extends JpaRepository<Space, Long> {
-}
